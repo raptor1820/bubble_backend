4c59729c4e3f9e4b3b3eec1060582683e9438a86
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2024-04-19 16:22:33 +0530

    first

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask,request
+from flask_cors import CORS
+from ml_utils import driver
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/")
+def root():
+    url  = request.args['url']
+    try:
+        return driver(url)
+    except:
+        return {"error": "error"}
+    
+if __name__== "__main__":
+    app.run()
